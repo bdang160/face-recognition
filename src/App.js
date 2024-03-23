23e867824f1a6226bf5fd72837d5465e2e24c8c4
Bryan Dang <azangi700@gmail.com>
2024-03-23 17:02:07 -0400

    Changes to get working again along with example img

--- src/App.js
+++ src/App.js
@@ -63,9 +63,76 @@ class App extends Component {
       console.log(this.state.imageUrl);
     });
 
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
-      .catch(err => console.log(err));
+    // Deprecated
+    // app.models.predict({
+    //   id: Clarifai.FACE_DETECT_MODEL,
+    //   version: "6dc7e46bc9124c5c8824be4822abe105",
+    // }, this.state.input)
+    //   .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    //   .catch(err => console.log(err));
+
+    //https://stackoverflow.com/questions/72567960/find-javascript-code-integrate-clarifai-face-detection-api
+    //help me => user_id can be found in multiple ways, one way is in https://portal.clarifai.com/settings/profile
+    const USER_ID = "azangi700";
+
+
+    // Your PAT (Personal Access Token) can be found in the portal under Authentification
+    // help me => PAT can be found in https://portal.clarifai.com/settings/authentication (create one if necessary!)
+    const PAT = "c2516e48f6be487f809d6a3c05533405";
+
+
+    // help me => App Id is just the name of your app on the portal.
+    const APP_ID = "6e5776a3667e49dc894cef3a933b9d0b";
+
+
+    // Change these to whatever model and image input you want to use
+    // help me => https://help.clarifai.com/hc/en-us/articles/1500007677141-Where-to-find-your-Model-IDs-and-Model-Version-IDs
+    const MODEL_ID = "face-detection";
+    const MODEL_VERSION_ID = "6dc7e46bc9124c5c8824be4822abe105";
+
+    const IMAGE_URL = this.state.input;
+
+    ///////////////////////////////////////////////////////////////////////////////////
+    // YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+    ///////////////////////////////////////////////////////////////////////////////////
+    const raw = JSON.stringify({
+      user_app_id: {
+        user_id: USER_ID,
+        app_id: APP_ID,
+      },
+      inputs: [
+        {
+          data: {
+            image: {
+              url: IMAGE_URL,
+            },
+          },
+        },
+      ],
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Key " + PAT,
+      },
+      body: raw,
+    };
+
+    fetch(
+        "https://api.clarifai.com/v2/models/" +
+        MODEL_ID +
+        "/versions/" +
+        MODEL_VERSION_ID +
+        "/outputs",
+        requestOptions
+    )
+        .then((response) => response.json())
+        .then((result) =>
+            this.displayFaceBox(this.calculateFaceLocation(result))
+        )
+        .catch((error) => console.log("error", error));
   }
 
   render() {
